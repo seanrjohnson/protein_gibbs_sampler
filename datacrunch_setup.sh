c38a7bcf950ece7fd432d1a25d567c9c7dd29d91
seanrjohnson <seanrjohnson@users.noreply.github.com>
2021-08-14 18:33:49 -0400

    add conda_env.yml update datacruch_setup.sh to use conda instead of docker.

--- datacrunch_setup.sh
+++ datacrunch_setup.sh
@@ -7,27 +7,22 @@ apt-get install fail2ban -y
 apt-get install ufw -y
 ufw allow ssh
 yes | ufw enable
-apt-get install -y docker.io make
 
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
-sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-sudo apt-get update
-apt-get install -y nvidia-container-runtime
-sudo systemctl restart docker
-
+# install conda
 cd /root
 HOME=/root
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh
+bash $HOME/miniconda.sh -b -p $HOME/miniconda
+eval "$(/root/anaconda/bin/conda shell.bash hook)" # this line might not be quite right.
+conda init
+
+# install protein_gibbs_sampler
 git clone --branch esm1b_followup_random_batch https://github.com/seanrjohnson/protein_gibbs_sampler.git
 
 cd protein_gibbs_sampler
-make run
 
-# then you can ssh in to the node 
-# make shell
-# conda install -c bioconda -y muscle
-# pip install -e .
-# then start the server.
-# jupyter lab --no-browser --ip=0.0.0.0 --allow-root
+conda create -f conda_env.yml
+
+# then when you log on:
+# conda activate protein_gibbs_sampler
