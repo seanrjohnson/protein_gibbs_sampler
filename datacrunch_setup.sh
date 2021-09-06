5e5012ad01b8131ec8b1d429ad9525474779439e
seanrjohnson <seanrjohnson@users.noreply.github.com>
2021-09-06 12:52:22 -0400

    update cloud setup

--- datacrunch_setup.sh
+++ datacrunch_setup.sh
@@ -14,17 +14,20 @@ HOME=/root
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh
 bash $HOME/miniconda.sh -b -p $HOME/miniconda
-eval "$(/root/anaconda/bin/conda shell.bash hook)" # this line might not be quite right.
+eval "$(/root/miniconda/bin/conda shell.bash hook)"
 conda init
 
-# install protein_gibbs_sampler
-git clone --branch esm1b_followup_random_batch https://github.com/seanrjohnson/protein_gibbs_sampler.git
+# install protein_gibbs_sampler and configure conda environment
+git clone --branch metrics https://github.com/seanrjohnson/protein_gibbs_sampler.git
 
 cd protein_gibbs_sampler
 
 conda activate base
 
-conda create -y -f conda_env.yml
+conda env create -f conda_env.yml
 
-# then when you log on:
-# conda activate protein_gibbs_sampler
+echo "conda activate protein_gibbs_sampler" >> $HOME/.bashrc
+
+# make a data directory, which will be at: /root/protein_gibbs_sampler/data
+
+mkdir data
